06abe57159cd300db8539a021afe53ec31693f80
Roy Martinez <155281719+Flimgees@users.noreply.github.com>
2026-04-27 22:43:52 -0700

    added loop

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,1 +1,16 @@
+let statistic = {
+    redCars: 21,
+    blueCars: 45,
+    greenCars: 12,
+    raceCars: 5,
+    blackCars: 40,
+    rareCars: 2
+};
 
+for (const property in statistic)
+{
+    if (property.startsWith('r') || (statistic[property] % 2 != 0))
+    {
+        console.log(statistic[property]);
+    }
+}
